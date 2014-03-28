e2b481b5c8f9a834fb4e621aa92778704c547486
MaciejMazur <maciejmazurx@gmail.com>
2014-03-28 22:33:19 +0100

    parallel implementation debug

--- interval_tree_parallel.cpp
+++ interval_tree_parallel.cpp
@@ -9,11 +9,146 @@ int range;
 int world_rank;
 int world_size;
 int n;
-bool test = false;
+int lvl_of_root;
 
-int* tree;
+// TREES
+int nr_of_trees;
+int** trees[22];
+int sub_size;
+
+void insert(int *tree, int x);
+int query(int* tree, int a, int b);
+//ROOT
+
+int shift;
 int size;
-void insert(int x){
+int* tree;
+int* procOwner;
+int* countNr;
+int* procCounts;
+
+void initialize(int pr_id, int count, int sub_range){
+  trees[pr_id] = new int *[count];
+  sub_size = 1;
+  while(sub_size<sub_range) sub_size *= 2;
+  for(int i = 0; i < count; i ++){
+    trees[pr_id][i] = new int[2*sub_size];
+    for(int j = 0; j < 2*sub_size; j ++)trees[pr_id][i][j] = 0;
+    printf("tree pr %d nr %d size %d\t",pr_id,i,sub_size*2); for(int j = 0; j < 2*sub_size; j++)printf("%d ",trees[pr_id][i][j]); printf("\n");
+  }
+}
+void insertR(int x){
+  int begin = 0, end = range - 1, v = 1, lvl = 1;
+  tree[v]++;
+  while(lvl<lvl_of_root){
+    int mid = begin + (end - begin)/2;
+    if(x>mid){
+      v = 2 * v + 1;
+      begin = mid + 1; 
+    } else {
+      v = 2 * v;
+      end = mid;
+    }
+    tree[v]++;
+    lvl++;
+  }
+  insert(trees[procOwner[v]][countNr[v]],x);
+}
+int lower(int x){
+  int begin = 0, end = range -1, v = 1, lvl = 1;
+  int res = 0;
+  while(lvl<lvl_of_root){
+    int mid = begin + (end - begin)/2;
+    if(x > mid) {
+      res += tree[2*v];
+      begin = mid + 1;
+      v = 2 * v + 1;
+    } else {
+      end = mid;
+      v = 2 * v;
+    }
+    lvl ++;
+  }
+
+  return res + query(trees[procOwner[v]][countNr[v]],begin,x-1);
+}
+
+int queryR(int a, int b){
+  return lower(b+1)-lower(a);
+}
+
+void parallel(){
+        //    MPI::COMM_WORLD.Send(buff, s, MPI_BYTE, j, 0);
+        //  MPI::COMM_WORLD.Recv(recv, s, MPI_BYTE, 0, 0, status);
+  if ( world_rank == 0){
+    size = 1;
+    for(int i = 0; i < lvl_of_root; i ++)size *= 2;
+    shift = size/2;   
+    printf("size %d shift %d \n", size, shift);
+    tree = new int[size];
+    for(int i = 0; i < size; i++) tree[i] = 0;
+    procOwner = new int[shift];
+    countNr = new int[shift];
+    procCounts = new int[proc];
+    for(int i = 0; i < proc; i++) procCounts[i] = 0;
+    for(int i = 0; i < shift; i++){
+      procOwner[i]=1+rand()%(proc-1);
+      procCounts[procOwner[i]]++;
+      countNr[i] = procCounts[procOwner[i]]-1;
+    }
+    printf("proc Owner "); for(int i = 0; i < shift; i++)printf("%d ",procOwner[i]); printf("\n");
+    printf("count Nr "); for(int i = 0; i < shift; i++)printf("%d ",countNr[i]); printf("\n");
+    printf("proc Counts "); for(int i = 0; i < proc; i++)printf("%d ",procCounts[i]); printf("\n");
+    for(int i = 1; i < proc; i++){
+      initialize(i,procCounts[i],range/shift);
+    } 
+    
+    scanf("%d",&n);
+    int a,b; 
+    for(int i = 0; i < n; i++){
+      scanf("%d %d",&a,&b);
+      if(a == -1){
+        insertR(b);
+      } else {
+        int sum = queryR(a,b);
+        printf("%d\n",sum);
+      }
+    }
+
+    
+
+    free(tree);
+    free(procOwner);
+
+
+  } else {
+    // NOT ROOT
+
+  }
+}
+
+int main(int argc, char** argv) {
+  MPI::Init();
+  world_rank=MPI::COMM_WORLD.Get_rank();
+  world_size=MPI::COMM_WORLD.Get_size();
+  proc = atoi(argv[1]);
+  printf("proc %d\n",proc);
+  range = atoi(argv[2]);
+  int w = 1; 
+  while(w < range) w *= 2;
+  range = w;
+  printf("range %d\n",range);
+  lvl_of_root = atoi(argv[3]);
+  printf("lvl_of_root %d\n", lvl_of_root);
+  double t0 = MPI_Wtime();
+  double t1 = 0; 
+  parallel();     
+  t1 = MPI_Wtime();
+//  printf("Proc: %d Range: %d Operations: %d Time: %f\n",proc,range,n, t1-t0);
+  MPI_Finalize();
+}
+
+void insert(int *tree, int x){
   int v = size+x;
   tree[v]++;
   while(v!=1){
@@ -22,7 +157,7 @@ void insert(int x){
   }
 }
 
-int query(int a, int b){
+int query(int* tree, int a, int b){
   int va = size+a;
   int vb = size+b;
   int wyn = tree[va];
@@ -34,105 +169,4 @@ int query(int a, int b){
   }
   return wyn;
 }
-void sequential(){
-  scanf("%d",&n);
-  if(test){
-    tree = new int[n];
-    for(int i = 0; i < n; i++)tree[i]=0;
-    int a,b;
-    for(int i = 0; i < n; i++){
-      scanf("%d %d",&a,&b);
-      if(a == -1){
-        tree[b]++;
-      } else {
-        int sum = 0;
-        for(int j = a; j <= b; j++)
-          sum+=tree[j];
-        printf("%d\n",sum);
-      }
-    }
-    free(tree);     
-  } else {
-    size = 1;
-    int a,b;
-    while(size<n) size = size * 2;
-    tree = new int[2 * size - 1];
-    for(int i = 0; i < 2 * size -1; i++)
-      tree[i]=0;
-    for(int i = 0; i < n; i++){
-      scanf("%d %d",&a,&b);
-      if(a == -1){
-        insert(b); 
-      } else {
-        int sum = query(a,b);
-        printf("%d\n",sum);
-      }
-    }
 
-    free(tree);
-  }
-}
-
-int* min_nodes;
-const int MAIN_TREE_LVL = 10;
-const int MAIN_TREEE_SIZE = 1024*2-1;
-void parallel(){
-        //    MPI::COMM_WORLD.Send(buff, s, MPI_BYTE, j, 0);
-        //  MPI::COMM_WORLD.Recv(recv, s, MPI_BYTE, 0, 0, status);
-  if ( world_rank == 0){
-    min_nodes = new int[proc];
-    int r = range / proc;
-    min_nodes[0]=0;
-    for(int i =1; i < proc; i ++)
-      min_nodes[i] = r + min_nodes[i-1];
-  
-    for(int i = 0; i < proc; i ++)
-      printf("%d ", min_nodes[i]);
-    printf("\n");
-   
-    tree = new int[MAIN_TREE_SIZE];
-    for(int i = 0; i < MAIN_TREE_SIZE; i ++)
-      tree[i] = 0;
-  
-    for(int i = 0; i < n; i++){
-      scanf("%d %d",&a,&b);
-      if(a == -1){
-        insert(b);
-      } else {
-        int sum = query(a,b);
-        printf("%d\n",sum);
-      }
-    }
-
-    
-
-    free(min_nodes);
-    free(tree);
-
-  } else {
-
-
-  }
-}
-
-int main(int argc, char** argv) {
-  MPI::Init();
-  world_rank=MPI::COMM_WORLD.Get_rank();
-  world_size=MPI::COMM_WORLD.Get_size();
-  
-  char isSequential = argv[1][0];
-  proc = atoi(argv[2]);
-  range = atoi(argv[3]);
-  double t0 = MPI_Wtime();
-  double t1 = 0; 
-  if (isSequential == 'T'){
-    sequential();
-  } else {
-    parallel();     
-  }
-  t1 = MPI_Wtime();
-
-//  printf("Proc: %d Range: %d Operations: %d Time: %f\n",proc,range,n, t1-t0);
-  
-  MPI_Finalize();
-}
